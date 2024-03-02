de81e38053372087b7461c8f7c34fc7fbf341331
jllewis11 <jllewis11@gmail.com>
2024-03-02 01:53:26 -0800

    added a unit test

---
+++ tests/test_key.py
@@ -0,0 +1,11 @@
+from testpilot.key import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def test_setkey():
+    example_key = '12345678901234567890'
+    setkey("openai", example_key )
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    assert openai_api_key == example_key
